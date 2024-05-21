26738ec0a7662e0087873bb12861c6fe6ead8257
Eilish Cuthbert <erfansafarali98@gmail.com>
2024-05-21 05:43:12 -0800

    initializing fuctions set up with exit function and main fucntion is now called in main.py

---
+++ func.py
@@ -0,0 +1,30 @@
+from art import *
+
+##############################[initializing fuctions]##################################
+def start_app():
+    art1 = text2art("Welcome", font='block',chr_ignore=True)
+    print(art1)
+    choose = input("Choose Your Login Method ... \n 1)Login \n 2)Signup \n 3)Exit \n ==>")
+
+    match choose:
+        case '1':
+            login()
+        case '2':
+            signup()
+        case '3':
+           exit()
+
+##############################[authentication functions]##################################
+
+def signup():
+    pass
+
+def login():
+    pass
+
+def exit():
+    exit()
+
+#############################
+
+start_app()
---
+++ main.py
@@ -0,0 +1,3 @@
+from func import *
+
+start_app()
